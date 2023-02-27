Fix quaternion rotation to be right-handed

geometry.py
@@ -177,13 +177,18 @@ class Point3(Vector):
         """
         return np.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
 
-    def rotate(self, q):
+    def rotate(self, rot):
         """
         Rotate point by quaternion
         """
-        vector_rot = q * self * q.inverse()
-        return Point3(vector_rot.view(np.ndarray)[0][0], vector_rot.view(np.ndarray)[0][1], vector_rot.view(np.ndarray)[0][2])
-        
+        if isinstance(rot, Quaternion):
+            q_temp = rot * self
+            vector_rot = q_temp * rot.inverse()
+            return Point3(vector_rot.view(np.ndarray)[0][1], vector_rot.view(np.ndarray)[0][2], vector_rot.view(np.ndarray)[0][3])
+        elif isinstance(rot, Rot3):
+            vector_rot = rot * self
+            return Point3(vector_rot.view(np.ndarray)[0][0], vector_rot.view(np.ndarray)[0][1], vector_rot.view(np.ndarray)[0][2])
+
 
 class Quaternion(Vector):
     """
@@ -193,10 +198,13 @@ class Quaternion(Vector):
         return Vector(np.array([[w, x, y, z]], dtype=float)).view(cls)
 
     @classmethod
-    def RzRyRx(cls, roll, pitch, yaw):
+    def RzRyRx(cls, yaw, pitch, roll):
         """
         Construct Quaternion given Euler angles
         """
+        yaw = -yaw
+        pitch = -pitch
+        roll = -roll
         cr = np.cos(roll / 2)
         sr = np.sin(roll / 2)
         cp = np.cos(pitch / 2)
@@ -253,6 +261,7 @@ class Quaternion(Vector):
 
     def __mul__(self, other):
         if isinstance(other, Quaternion):
+            # Hamilton product
             return Quaternion(w=self.w * other.w - self.x * other.x - self.y * other.y - self.z * other.z,
                               x=self.w * other.x + self.x * other.w + self.y * other.z - self.z * other.y,
                               y=self.w * other.y - self.x * other.z + self.y * other.w + self.z * other.x,
@@ -260,24 +269,20 @@ class Quaternion(Vector):
         elif isinstance(other, Point3):
             return self * Quaternion(0, other.x, other.y, other.z)
         else:
-            return Quaternion(self.w * other, self.x * other, self.y * other, self.z * other)
+            return Vector(np.array([self.w * other[0], self.x * other[1], self.y * other[2], self.z * other[3]], dtype=float))
 
     def __rmul__(self, other):
-        if isinstance(other, Quaternion):
-            return Quaternion(w=self.w * other.w - self.x * other.x - self.y * other.y - self.z * other.z,
-                              x=self.w * other.x + self.x * other.w + self.y * other.z - self.z * other.y,
-                              y=self.w * other.y - self.x * other.z + self.y * other.w + self.z * other.x,
-                              z=self.w * other.z + self.x * other.y - self.y * other.x + self.z * other.w)
-        elif isinstance(other, Point3):
+        if isinstance(other, Point3):
             return Quaternion(0, other.x, other.y, other.z) * self
         else:
-            return Quaternion(self.w * other, self.x * other, self.y * other, self.z * other)
+            return Vector(np.array([self.w * other[0], self.x * other[1], self.y * other[2], self.z * other[3]], dtype=float))
 
     def inverse(self):
         """
         Returns the inverse of the quaternion
         """
-        return Quaternion(self.w, -self.x, -self.y, -self.z)
+        norm = self.norm()
+        return Quaternion(self.w/norm, -self.x/norm, -self.y/norm, -self.z/norm)
 
     def norm(self):
         """
@@ -432,7 +437,7 @@ class Rot3(np.ndarray):
         """
         Rotates a point by the rotation matrix
         """
-        rot_p = self @ point.transpose()
+        rot_p = np.dot(self.inverse(), point.transpose())
         return Point3(rot_p[0][0], rot_p[1][0], rot_p[2][0])
 
     def roll(self):
@@ -533,6 +538,10 @@ class Pose3:
     def transform_to(self, pose):
         """
         Transforms a pose to another pose
+
+        Convention:
+        - T_B_A : Transform to frame B from frame A
+        - Transform is defined as a translation followed by rotation
         """
         if isinstance(pose, Pose3):
             R_ = self.R.inverse() * pose.R
@@ -554,9 +563,22 @@ class Pose3:
     
 
 class Transform:
+    """
+    Represents a 3D transform between two frames
+    
+    Nomenclature:
+        T_B_A : Transform to frame B from frame A
+    
+    Example:
+        T_n_a = T_b_a * T_c_b * ... * T_n_n-1
+
+        PoseA which is in frame A is transformed to frame B by T_b_a
+        PoseB = T_b_a * PoseA
+    """
+
     def __init__(self, from_frame: str, to_frame: str, t=Point3(), R=Quaternion()):
-        self.from_frame = from_frame
         self.to_frame = to_frame
+        self.from_frame = from_frame
         self.t = t
         self.R = R
 

tests.py
@@ -129,6 +129,30 @@ class TestPoint3(unittest.TestCase):
 
         self.assertEqual(dist, p1.distance(p2))
 
+    def test_rotate(self):
+        p = Point3(1, 0, 0)
+
+        # Rotate using a quaternion
+        q1 = Quaternion.RzRyRx(np.pi/2, 0, 0)
+        q1.normalise()
+        p1 = p.rotate(q1)
+        self.assertTrue(np.isclose(p1.x, 0))
+        self.assertTrue(np.isclose(p1.y, -1))
+        self.assertTrue(np.isclose(p1.z, 0))
+       
+        # Rotate using a rotation matrix
+        rot = Rot3.RPY(0, 0, -np.pi/2)
+        p2 = p.rotate(rot)
+
+        self.assertTrue(np.isclose(p2.x, 0))
+        self.assertTrue(np.isclose(p2.y, 1))
+        self.assertTrue(np.isclose(p2.z, 0))
+
+        # Rotate back
+        p3 = p2.rotate(rot.inverse())
+        self.assertTrue(np.isclose(p3.x, 1))
+        self.assertTrue(np.isclose(p3.y, 0))
+        self.assertTrue(np.isclose(p3.z, 0))
 
 class TestQuaternion(unittest.TestCase):
     def test_init(self):
@@ -186,9 +210,9 @@ class TestQuaternion(unittest.TestCase):
         q = Quaternion.RzRyRx(np.pi/2, 0, 0)
         q.normalise()
         p_rot = p.rotate(q)
-        
+       
         self.assertTrue(np.isclose(p_rot.x, 0))
-        self.assertTrue(np.isclose(p_rot.y, 1))
+        self.assertTrue(np.isclose(p_rot.y, -1))
         self.assertTrue(np.isclose(p_rot.z, 0))
 
 
@@ -250,6 +274,18 @@ class TestRot3(unittest.TestCase):
         result = rot * rot_inv
         self.assertTrue(np.array_equal(result, np.identity(n=3, dtype=float)))
 
+    def test_rot(self):
+        # Rotate point and then rotate back to original
+        point = Point3(np.random.rand(), np.random.rand(), np.random.rand())
+        rot = Rot3.RPY(np.random.rand(), np.random.rand(), np.random.rand())
+
+        point_rot = rot * point
+        point_orig = rot.inverse() * point_rot
+
+        self.assertTrue(np.isclose(point.x, point_orig.x))
+        self.assertTrue(np.isclose(point.y, point_orig.y))
+        self.assertTrue(np.isclose(point.z, point_orig.z))
+
 
 class TestPose3(unittest.TestCase):
     def test_init(self):
@@ -286,8 +322,6 @@ class TestPose3(unittest.TestCase):
         poseB = Pose3(rotB, tB)
 
         poseC = poseA.compose(poseB)
-        print(poseC.R.to_euler())
-        print(poseC.t)
 
     def test_multiply(self):
         rotA = Rot3.RPY(0, 0, 0)
@@ -305,6 +339,9 @@ class TestPose3(unittest.TestCase):
         self.assertTrue(poseC.t.z == poseB.t.z + poseA.t.z)
 
     def test_transform(self):
+        """ 
+        Transform applies translation first then rotation
+        """
         rotA = Rot3.RPY(np.pi/4, 0, 0)
         tA = Point3(0, 0, 1)
         poseA = Pose3(rotA, tA)
@@ -322,8 +359,8 @@ class TestPose3(unittest.TestCase):
         self.assertTrue(np.isclose(rot_eul.z, 0))
 
         self.assertTrue(np.isclose(trans.x, 0))
-        self.assertTrue(np.isclose(trans.y, np.sqrt(2)))
-        self.assertTrue(np.isclose(trans.z, 0))
+        self.assertTrue(np.isclose(trans.y, 0))
+        self.assertTrue(np.isclose(trans.z, -np.sqrt(2)))
 
 
 if __name__ == "__main__":