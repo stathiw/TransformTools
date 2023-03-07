2694b38768fc406dc4812fae04454a381ca24dc6
Stathi Weir <stathi.weir@gmail.com>
2023-03-08 00:13:32 +1100

    Clean up internal method names

--- SearchTree.py
+++ SearchTree.py
@@ -11,7 +11,7 @@ class SearchTree:
     def __init__(self, root):
         self.root = root
     
-    def search(self, name, node=None):
+    def _search(self, name, node=None):
         """
         Return the node with the given name if it exists
         """
@@ -21,7 +21,7 @@ class SearchTree:
         if node.name == name:
             return node
         for child in node.children:
-            node = self.search(name, child)
+            node = self._search(name, child)
             if node != None:
                 return node
         # Exhausted all children, return None
@@ -31,7 +31,7 @@ class SearchTree:
         """
         Add a child node to frame with name parent_name
         """
-        parent_node = self.search(parent_name)
+        parent_node = self._search(parent_name)
         print("Adding %s to %s" % (child_node.name, parent_node.name))
         if parent_node != None:
             child_node.parent = parent_node
@@ -39,7 +39,7 @@ class SearchTree:
         else:
             print("Parent node %s not found" % parent_name)
 
-    def get_path_to_parent(self, node, parent=None):
+    def _get_path_to_parent(self, node, parent=None):
         """
         Return a list of nodes from the parent to the node
         """
@@ -54,12 +54,12 @@ class SearchTree:
         path.append(parent)
         return path
 
-    def get_common_ancestor(self, node1, node2):
+    def _get_common_ancestor(self, node1, node2):
         """
         Return the deepest common ancestor of two nodes
         """
-        path1 = self.get_path_to_parent(node1)
-        path2 = self.get_path_to_parent(node2)
+        path1 = self._get_path_to_parent(node1)
+        path2 = self._get_path_to_parent(node2)
         for node in path1:
             if node in path2:
                 return node
@@ -69,21 +69,29 @@ class SearchTree:
         """
         Return the distance between two nodes
         """
-        common_ancestor = self.get_common_ancestor(node1, node2)
+        common_ancestor = self._get_common_ancestor(node1, node2)
         # Get distance from node1 to common ancestor
         
     def get_path(self, from_node, to_node):
         """
         Return a list of nodes from from_node to to_node
         """
+        from_node = self._search(from_node)
+        to_node = self._search(to_node)
+        if from_node == None:
+            print("From node not found")
+            return None
+        if to_node == None:
+            print("To node not found")
+            return None
         # Find common ancestor
-        common_ancestor = self.get_common_ancestor(from_node, to_node)
+        common_ancestor = self._get_common_ancestor(from_node, to_node)
         if common_ancestor == None:
             return None
         # Get path from from_node to common ancestor
-        path1 = self.get_path_to_parent(from_node, common_ancestor)
+        path1 = self._get_path_to_parent(from_node, common_ancestor)
         # Get path from common ancestor to to_node
-        path2 = self.get_path_to_parent(to_node, common_ancestor)
+        path2 = self._get_path_to_parent(to_node, common_ancestor)
         # Remove last element from path2 to avoid duplication of common ancestor node
         path2.pop()
         # Reverse path2
@@ -92,7 +100,7 @@ class SearchTree:
         path = path1 + path2
         return path
 
-    def get_max_depth(self, node, depth=0):
+    def _get_max_depth(self, node, depth=0):
         """
         Return the depth of a node
         """
@@ -103,7 +111,7 @@ class SearchTree:
         max_depth = depth
         for child in node.children:
             # Recursively explore children and update max_depth
-            depth_of_child = self.get_max_depth(child, depth + 1)
+            depth_of_child = self._get_max_depth(child, depth + 1)
             if depth_of_child > max_depth:
                 max_depth = depth_of_child
         return max_depth
@@ -133,21 +141,36 @@ if __name__ == "__main__":
     tree.add("C", Node("F", val=0))
     tree.add("B", Node("G", val=0))
 
-    print("Max depth of tree: %d" % tree.get_max_depth(tree.root, 0))
+    print("Max depth of tree: %d" % tree._get_max_depth(tree.root, 0))
 
     tree.print_tree(tree.root)
 
-    path_to_G = tree.get_path_to_parent(tree.search("G"))
-    path_to_E = tree.get_path_to_parent(tree.search("E"), tree.root)
+    node_G = tree._search("G")
+    node_E = tree._search("E")
+
+    if node_G == None:
+        print("Node G not found")
+
+    if node_E == None:
+        print("Node E not found")
+
+    path_to_G = tree._get_path_to_parent(tree._search("G"))
+    path_to_E = tree._get_path_to_parent(tree._search("E"), tree.root)
     
     for node in path_to_G:
         print(" " + node.name)
     for node in path_to_E:
         print(" " + node.name)
 
-    common_ancestor = tree.get_common_ancestor(tree.search("G"), tree.search("E"))
+    common_ancestor = tree._get_common_ancestor(tree._search("G"), tree._search("E"))
 
-    tf_path = tree.get_path(tree.search("G"), tree.search("F"))
+    tf_path = tree.get_path("D", "F")
     print("Path from G to F: ", end="")
     for node in tf_path:
-        print(node.name)
+        print(node.name, end="")
+        if node != tf_path[-1]:
+            print(" -> ", end="")
+        else:
+            print()
+
+    path = tree.get_path("G", "P")
